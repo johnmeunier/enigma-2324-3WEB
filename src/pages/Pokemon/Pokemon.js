1ba6dbca17d8b73128b992947d674dad9d0d5d4c
Johnathan MEUNIER <johnathan2693@gmail.com>
2024-01-31 14:26:12 +0100

    fea(pkm): limit

--- src/pages/Pokemon/Pokemon.js
+++ src/pages/Pokemon/Pokemon.js
@@ -1,19 +1,27 @@
 import { useState, useEffect } from 'react';
 
 export const Pokemon = () => {
+  const [pokemons, setPokemons] = useState({});
+  const [limit, setLimit] = useState(10);
+
   useEffect(() => {
     async function fetchData() {
-      const promiseGetAllPokemons = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+      const promiseGetAllPokemons = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
       const getAllPokemonsJSON = await promiseGetAllPokemons.json();
       setPokemons(getAllPokemonsJSON);
     }
     fetchData();
-  }, []);
+  }, [limit]);
 
-  const [pokemons, setPokemons] = useState({});
 
   return <>
     <h2>Pokemon</h2>
+    <select name="limit" id="limit" onChange={(e) => setLimit(e.target.value)}>
+      <option value="10">10</option>
+      <option value="20">20</option>
+      <option value="50">50</option>
+      <option value="100">100</option>
+    </select>
     <h3>Compteur : {pokemons.results?.length}</h3>
     {pokemons.results 
     ? 
