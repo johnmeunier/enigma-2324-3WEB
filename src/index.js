edd82b5d67bb14cb66ea6137286e8d6a3ce901a1
Johnathan MEUNIER <johnathan2693@gmail.com>
2024-02-14 14:25:04 +0100

    refacto(Router): router in a specific component

--- src/index.js
+++ src/index.js
@@ -1,40 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { GameOfLife } from "./pages/GameOfLife";
-import { Pokemon, PokemonDetails } from "./pages/Pokemon";
-
+import { Router } from "./pages/Router";
 import { worker } from "./mocks/browser";
 
-import App from "./App";
 import "./index.css";
 
 worker.start();
 
-const router = createBrowserRouter([
-  {
-    element: <App />,
-    errorElement: <h1> Error </h1>,
-    children: [
-      {
-        path: "/",
-        element: <h2>Home</h2>,
-      },
-      {
-        path: "game-of-life",
-        element: <GameOfLife />,
-      },
-      {
-        path: "pokemon",
-        element: <Pokemon />,
-      },
-      {
-        path: "pokemon/:name",
-        element: <PokemonDetails />,
-      },
-    ],
-  },
-]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(Router);
