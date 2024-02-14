62068c3534617a6a47ec5246ea156ffa339c87f2
Johnathan MEUNIER <johnathan2693@gmail.com>
2024-02-14 16:07:54 +0100

    test: implement test

--- src/App.test.js
+++ src/App.test.js
@@ -1,33 +1,47 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, act, within, waitFor } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test("render homepage", () => {
-  // given
+test("render home page", () => {
   render(<App />);
-  // when
-  // then
-  expect(screen.getByText(/Home page/i)).toBeInTheDocument();
+  expect(screen.getByText("Homepage")).toBeInTheDocument();
 });
 
-test("render pokemon list page", async () => {
-  // given
+test("render pokemon list", async () => {
   render(<App />);
-  // when
-  const $pokemonListLink = screen.getByRole("link", { name: /Liste/i });
-  $pokemonListLink.click();
-  // then
-  expect(await screen.findByText("Pokedex")).toBeInTheDocument();
+  const $link = screen.getByRole("link", { name: /Liste/i });
+  act(() => {
+    $link.click();
+  });
+  expect(
+    await screen.findByRole("heading", { level: 2, name: "Pokedex" })
+  ).toBeInTheDocument();
 });
 
-test("render on pokemon list page 10 pokemons", async () => {
-  expect($pokemonList.length).toEqual(10);
+test("render pokemon details", async () => {
+  render(<App />);
+  const $link = screen.getByRole("link", { name: /Liste/i });
+  act(() => {
+    $link.click();
+  });
+  const $pokemon = await screen.findByRole("link", { name: /bulbasaur/i });
+  act(() => {
+    $pokemon.click();
+  });
+  expect(await screen.findByText("bulbasaur")).toBeInTheDocument();
 });
 
-test("render on pokemon list page 50 pokemons when the user change select value", async () => {
-  expect($pokemonList.length).toEqual(50);
-});
-
-test("render pokemon detail", async () => {
-  expect(await screen.findByRole("heading", { level: 2, name: "squirtle" }))
-    .toBeInTheDocument;
+test("display 50 pokemons", async () => {
+  render(<App />);
+  const $link = screen.getByRole("link", { name: /Liste/i });
+  act(() => {
+    $link.click();
+  });
+  const $select = screen.getByRole("combobox");
+  userEvent.selectOptions($select, "50");
+  await waitFor(() => {
+    const $listPokemons = screen.getAllByRole("list")[1];
+    const $options = within($listPokemons).getAllByRole("listitem");
+    expect($options).toHaveLength(50);
+  });
 });
