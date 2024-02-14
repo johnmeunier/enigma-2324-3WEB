d0742d74721e8ebfe5125c5c2aa2b3c1cd925133
Johnathan MEUNIER <johnathan2693@gmail.com>
2024-02-14 14:43:30 +0100

    test: init test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("render homepage", () => {
+  // given
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // when
+  // then
+  expect(screen.getByText(/Home page/i)).toBeInTheDocument();
+});
+
+test("render pokemon list page", async () => {
+  // given
+  render(<App />);
+  // when
+  const $pokemonListLink = screen.getByRole("link", { name: /Liste/i });
+  $pokemonListLink.click();
+  // then
+  expect(await screen.findByText("Pokedex")).toBeInTheDocument();
+});
+
+test("render on pokemon list page 10 pokemons", async () => {
+  expect($pokemonList.length).toEqual(10);
+});
+
+test("render on pokemon list page 50 pokemons when the user change select value", async () => {
+  expect($pokemonList.length).toEqual(50);
+});
+
+test("render pokemon detail", async () => {
+  expect(await screen.findByRole("heading", { level: 2, name: "squirtle" }))
+    .toBeInTheDocument;
 });
